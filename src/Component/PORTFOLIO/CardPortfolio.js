9e7f63030bb7fff131e6e0786cf8522fae4f79b6
moein <Moein.kh.dark.siders@gmail.com>
2024-07-20 17:40:14 +0330

    sd

--- src/Component/PORTFOLIO/CardPortfolio.js
+++ src/Component/PORTFOLIO/CardPortfolio.js
@@ -1,4 +1,13 @@
-              <Container fluid>
+import React from 'react'
+import { Container, Col } from 'react-bootstrap'
+import { FaEye } from "react-icons/fa";
+export default function CardPortfolio(props) {
+    return (
+        <>
+            <Col xxl={4} xl={6} lg={6} xs={12} >
+                <div className='cardwrapper' style={{ backgroundImage: `url(${props.images})` }} >
+
+                    <Container fluid>
                         <div className="cardfooter">
                             <h3 className='cardtitle'>{props.title}</h3>
                             <p className='cardsubject'>{props.desc}</p>
